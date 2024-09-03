649471d01ded69ab6e531036d970d17ab3886bab
Tim Ermilov <yamalight@gmail.com>
2024-09-03 12:45:41 +0200

    Improve result rendering

--- app/components/pipeline/output/Results.tsx
+++ app/components/pipeline/output/Results.tsx
@@ -1,36 +1,11 @@
-import { Doc } from '@/src/doc/Document';
+import { StepResult } from '@/src/step/Step';
 import { CustomMarkdown } from '../../markdown/Markdown';
 
-export function RenderResults({ results }: { results: Doc[] | Doc }) {
-  if (Array.isArray(results)) {
-    return (
-      <>
-        <div className="prose prose-sm dark:prose-invert w-full max-w-full">
-          {results.map((res) => (
-            <div key={res.id} className="mb-4">
-              <h1>Doc: {res.name}</h1>
-              <div className="divide-y divide-neutral-600/50">
-                {res.processingResults.map((r) => (
-                  <div key={r.stepId}>
-                    <CustomMarkdown>{r?.result}</CustomMarkdown>
-                  </div>
-                ))}
-              </div>
-            </div>
-          ))}
-        </div>
-      </>
-    );
-  }
-
-  const result = results.processingResults;
-
+export function RenderResults({ result }: { result: StepResult }) {
   return (
     <>
       <div className="prose prose-sm dark:prose-invert w-full max-w-full">
-        {result.map((r) => (
-          <CustomMarkdown key={r.stepId}>{r?.result}</CustomMarkdown>
-        ))}
+        <CustomMarkdown>{result.result}</CustomMarkdown>
       </div>
     </>
   );
--- app/components/pipeline/output/basic/Basic.tsx
+++ app/components/pipeline/output/basic/Basic.tsx
@@ -1,18 +1,33 @@
-import { OutputStep } from '@/src/step/Step';
+import { pipelineAtom } from '@/app/store/store';
+import { OutputStep, StepResult } from '@/src/step/Step';
+import { useAtomValue } from 'jotai';
 import { useMemo } from 'react';
 import { RenderResults } from '../Results';
 import { BasicOutputConfig } from '../types';
 
 export function BasicOutput({ data }: { data: OutputStep }) {
+  const pipeline = useAtomValue(pipelineAtom);
   const config = useMemo(() => data.config as BasicOutputConfig, [data]);
+  const result = useMemo(() => {
+    let results: StepResult[] = [];
+    if (Array.isArray(config.results)) {
+      results = config.results.map((r) => r.processingResults).flat();
+    } else {
+      results = config.results.processingResults;
+    }
+
+    const lastStep = pipeline.output.id;
+    const res = results.find((r) => {
+      const step = pipeline.steps.find((s) => s.id === r.stepId);
+      return step?.connectsTo.includes(lastStep);
+    });
+    console.log({ lastStep, res });
+    return res;
+  }, [config, pipeline]);
 
   return (
     <div className="flex flex-col w-full h-full overflow-auto">
-      {config.results ? (
-        <RenderResults results={config.results} />
-      ) : (
-        <>No results</>
-      )}
+      {result ? <RenderResults result={result} /> : <>No results</>}
     </div>
   );
 }
