024ffd10c0210aee2cdf2f0e13760736391a35d4
Tim Ermilov <yamalight@gmail.com>
2024-09-30 14:27:20 +0200

    Improve page title

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -3,7 +3,7 @@ import { UI } from '~/components/ui/UI';
 
 export const meta: MetaFunction = () => {
   return [
-    { title: 'LitLytics' },
+    { title: 'LitLytics - affordable, simple analytics platform for everyone' },
     {
       name: 'description',
       content: `LitLytics is an affordable, simple analytics platform for everyone designed to streamline the process of working with complex datasets.
