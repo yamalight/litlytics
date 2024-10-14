018d81ed7479bc906a29c2cb8050ae3b1341205d
Tim Ermilov <yamalight@gmail.com>
2024-10-14 11:58:44 +0200

    Fix linting issue for litlytics package build script

--- packages/litlytics/build.ts
+++ packages/litlytics/build.ts
@@ -1,3 +1,6 @@
+// ignore ban ts comment and ignore top level await
+/* eslint @typescript-eslint/ban-ts-comment: 0 */
+// @ts-ignore
 await Bun.build({
   entrypoints: ['./litlytics.ts'],
   outdir: './dist',
