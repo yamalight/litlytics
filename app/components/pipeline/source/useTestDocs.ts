e995baff960667ae226285d9051180144b26718a
Tim Ermilov <yamalight@gmail.com>
2024-10-16 14:34:35 +0200

    Fix issue with source docs being empty

--- app/components/pipeline/source/useTestDocs.ts
+++ app/components/pipeline/source/useTestDocs.ts
@@ -3,7 +3,10 @@ import { useLitlytics } from '~/store/WithLitLytics';
 
 export function useTestDocs() {
   const { pipeline } = useLitlytics();
-  const allDocs = useMemo(() => pipeline.source.docs, [pipeline.source.docs]);
+  const allDocs = useMemo(
+    () => pipeline.source.docs ?? [],
+    [pipeline.source.docs]
+  );
   const testDocs = useMemo(() => allDocs.filter((d) => d?.test), [allDocs]);
 
   return { allDocs, testDocs };
